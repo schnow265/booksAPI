ad4dd43e27e76ed2d42f80054a161eb096c6bfa3
schnow265 <thesnowbox@icloud.com>
2024-06-15 21:16:00 +0200

    init

---
+++ src/main/java/at/schnow265/booksAPI/BooksApiApplication.java
@@ -0,0 +1,13 @@
+package at.schnow265.booksAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/schnow265/booksAPI/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.schnow265.booksAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class BooksApiApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
