4340cd94b793b0880cb7648c34254f51a6bcb9df
schnow265 <thesnowbox@icloud.com>
2024-06-16 20:40:16 +0200

    used a cheeky hack from the baeldung tutorial for java.util.concurrent.Future.
    I am not kidding.

--- src/main/java/at/schnow265/booksAPI/BooksApiApplication.java
+++ src/main/java/at/schnow265/booksAPI/BooksApiApplication.java
@@ -2,28 +2,11 @@ package at.schnow265.booksAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
-@EnableAsync
 public class BooksApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApiApplication.class, args);
-	}
-
-	@Bean(name = "threadPoolTaskExecutor")
-	public Executor taskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(2);
-		executor.setMaxPoolSize(2);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("threadPool-");
-		executor.initialize();
-		return executor;
 	}
 }
